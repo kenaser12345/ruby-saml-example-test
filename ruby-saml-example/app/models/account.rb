c0aa133ef64b9c3304cd6bf9779fe33d80373144
Ken <53910941+kenaser12345@users.noreply.github.com>
2022-03-13 23:18:22 +0800

    Add files via upload

---
+++ ruby-saml-example/app/models/account.rb
@@ -0,0 +1,43 @@
+class Account < ActiveRecord::Base
+  def self.get_saml_settings(url_base)
+    # this is just for testing purposes.
+    # should retrieve SAML-settings based on subdomain, IP-address, NameID or similar
+    settings = OneLogin::RubySaml::Settings.new
+
+    url_base ||= "http://localhost:3000"
+
+    # Example settings data, replace this values!
+
+    # When disabled, saml validation errors will raise an exception.
+    settings.soft = true
+
+    #SP section
+    settings.issuer                         = url_base + "/saml/metadata"
+    settings.assertion_consumer_service_url = url_base + "/saml/acs"
+    settings.assertion_consumer_logout_service_url = url_base + "/saml/logout"
+
+    onelogin_app_id = "test_login_id"
+
+    # IdP section
+    settings.idp_entity_id                  = "http://localhost:3002/saml/metadata"
+    settings.idp_sso_target_url             = "http://localhost:3002/saml/auth"
+    settings.idp_slo_target_url             = "http://localhost:3002/saml/logout"
+    settings.idp_cert                       = ""
+
+
+    settings.idp_cert_fingerprint           = "9E:65:2E:03:06:8D:80:F2:86:C7:6C:77:A1:D9:14:97:0A:4D:F4:4D"
+    settings.idp_cert_fingerprint_algorithm = XMLSecurity::Document::SHA1
+
+    settings.name_identifier_format         = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+
+    # Security section
+    settings.security[:authn_requests_signed] = false
+    settings.security[:logout_requests_signed] = false
+    settings.security[:logout_responses_signed] = false
+    settings.security[:metadata_signed] = false
+    settings.security[:digest_method] = XMLSecurity::Document::SHA1
+    # settings.security[:signature_method] = XMLSecurity::Document::RSA_SHA1
+
+    settings
+  end
+end
